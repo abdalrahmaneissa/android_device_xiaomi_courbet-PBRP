b341d8397cfe806b8153ce28acb4761826b0b6ed
Abdalrahman <abdalrahmaneissaa@proton.me>
2025-11-21 21:40:10 +0200

    AndroidProducts.mk: Add PBRP lunchunch combos
    - Define PRODUCT_MAKEFILES for pb_courbet.mk
    - Add standard PBRP lunch choices: user, userdebug, eng
    - Use official PBRP naming convention (pb_ prefix)

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/pb_courbet.mk
+
+COMMON_LUNCH_CHOICES := \
+    pb_courbet-user \
+    pb_courbet-userdebug \
+    pb_courbet-eng
